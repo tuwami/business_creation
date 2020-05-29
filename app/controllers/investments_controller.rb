0380a66c98f85ef2972c746bdb36a18179db2c56
NoharaMasato <masato338914914@gmail.com>
2020-05-30 00:35:55 +0900

    renzoku investment

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -11,19 +11,32 @@ class InvestmentsController < ApplicationController
   
   def create
     if user_signed_in?
-      current_user.team.markets.each do |market|
+      @team = current_user.team
+      @users = @team.users
+      @team.markets.each do |market|
         if params[:investment][:market_id].to_i == market.id
           investment = Investment.new(investment_params)
         else
           investment = Investment.new(market_id: market.id, budget: 0, assigning: 0)
         end
         investment.team_id = params[:team_id]
-        if investment.save
-          investment.calculate_team_status # calc_fund, calc_employee, calc_novice
-          investment.calculate_market_status
-          investment.save_history
+        if @team.investments.length == 0 || Time.zone.now - @team.investments.last.created_at >= 60
+          if investment.save
+            investment.calculate_team_status # calc_fund, calc_employee, calc_novice
+            investment.calculate_market_status
+            investment.save_history
+          else
+            @investment = Investment.new
+            @use_histories = @team.histories.where(market_id: @team.markets.last.id)
+            render 'teams/show'
+            return
+          end
         else
-          render 'team/show'
+          @investment = Investment.new
+          @use_histories = @team.histories.where(market_id: @team.markets.last.id)
+          flash[:alert] = 'Save error!'
+          render 'teams/show'
+          return
         end
       end
       redirect_to team_path(params[:team_id]), notice: 'Success!'
