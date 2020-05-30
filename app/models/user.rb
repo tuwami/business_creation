c507d995889d301487cc1592a540506de032d1df
NoharaMasato <masato338914914@gmail.com>
2020-05-30 16:00:19 +0900

    signup dekinai

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   belongs_to :team, optional: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 end
