70724fd257244fcaca0952bea77dc5eeee5df1ee
tuwami <taiseiuwami@gmail.com>
2020-06-02 14:06:22 +0000

    organize logic part2

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -60,13 +60,12 @@ class Investment < ApplicationRecord
   def cal_params_market_recruiting
     if market.market_master_id == 1
       if market.market_employee < 10
-        ret_val = cal_market_recruiting(5,5)
+        return cal_market_recruiting(5,5)
       elsif market.market_employee < 100
-        ret_val = cal_market_recruiting(10,10)
+        return cal_market_recruiting(10,10)
       else
-        ret_val = cal_market_recruiting(30,30)
+        return cal_market_recruiting(30,30)
       end
-      return ret_val
     else
       return 0
     end
@@ -80,10 +79,10 @@ class Investment < ApplicationRecord
   EATH_MARKET_VALUES_D = [0.086,7.59,1.0050]
   EATH_MARKET_VALUES_E = [0.055,2.35,1.0084]
   EATH_MARKET_VALUES_F = [0.164,4.85,1.0108]
-  EATH_MARKET_VALUES_G = [0.05,1.72,0.9986]
+  EATH_MARKET_VALUES_G = [0.050,1.72,0.9986]
   EATH_MARKET_VALUES_H = [0.152,2.36,1.0074]
-  EATH_MARKET_VALUES_I = [0.05,6.44,1.0034]
-  EATH_MARKET_VALUES_J = [0.168,4.59,1.01]
+  EATH_MARKET_VALUES_I = [0.050,6.44,1.0034]
+  EATH_MARKET_VALUES_J = [0.168,4.59,1.0100]
   EATH_MARKET_VALUES_K = [0.084,5.37,1.0013]
   EATH_MARKET_VALUES_L = [0.086,3.20,1.0041]
   EATH_MARKET_VALUES_M = [0.113,1.69,1.0016]
@@ -93,24 +92,63 @@ class Investment < ApplicationRecord
   EATH_MARKET_VALUES_Q = [0.074,8.08,1.0023]
   EATH_MARKET_VALUES_R = [0.020,0.03,1.0025]
   
-  MARKET_VALUES = [EATH_MARKET_VALUES_B, EATH_MARKET_VALUES_C, EATH_MARKET_VALUES_D, 
-                  EATH_MARKET_VALUES_E, EATH_MARKET_VALUES_F, EATH_MARKET_VALUES_G, 
-                  EATH_MARKET_VALUES_H, EATH_MARKET_VALUES_I, EATH_MARKET_VALUES_J, 
-                  EATH_MARKET_VALUES_K, EATH_MARKET_VALUES_L, EATH_MARKET_VALUES_M, 
-                  EATH_MARKET_VALUES_N, EATH_MARKET_VALUES_O, EATH_MARKET_VALUES_P, 
-                  EATH_MARKET_VALUES_Q, EATH_MARKET_VALUES_R]
+  MARKET_VALUES = [ EATH_MARKET_VALUES_B, EATH_MARKET_VALUES_C, EATH_MARKET_VALUES_D, 
+                    EATH_MARKET_VALUES_E, EATH_MARKET_VALUES_F, EATH_MARKET_VALUES_G, 
+                    EATH_MARKET_VALUES_H, EATH_MARKET_VALUES_I, EATH_MARKET_VALUES_J, 
+                    EATH_MARKET_VALUES_K, EATH_MARKET_VALUES_L, EATH_MARKET_VALUES_M, 
+                    EATH_MARKET_VALUES_N, EATH_MARKET_VALUES_O, EATH_MARKET_VALUES_P, 
+                    EATH_MARKET_VALUES_Q, EATH_MARKET_VALUES_R ]
+  
+  BALANCE_VALUES = [0.3,0.3,0.5,0.95,0.4,0.95,0.4,0.95,0.3,0.3,0.3,0.3,0.3,0.3,0.8,0.9,0.5,0.3,0.3]
   
   def cal_params_market_earning
-    if market.market_master_id = 1
+    if market.market_master_id == 1
       return 0
     else
       (2..18).each do |num|
         if num == market.market_master_id
-          return cal_market_earning(MARKET_VALUES[num - 2])
+          return cal_market_earning(*MARKET_VALUES[num - 2])
         end
       end
     end
   end
+  
+  def cal_params_balance
+    if market.market_master_id == 1
+      return 0
+    else
+      (2..18).each do |num|
+        if num == market.market_master_id
+          return cal_balance(BALANCE_VALUES[num - 2])
+        end
+      end
+    end
+  end
+  
+  def cal_market_recruiting(a,b) #a = 資本の効率性,b = 人的リソースの効率性
+    return Math.log(1 + ((budget*a)*((market.market_employee + assigning)*b*rand(5000..15000)) / 10000))
+  end
+
+  def cal_market_earning(a,b,c) #a = 資本の効率性,b = 人的リソースの効率性,c = 市場の成長性
+    if market.market_employee < 10
+      return Math.log(1 + (MARKETSIZE[market.market_master_id-2]*(MARKETSHARE[market.market_master_id-2]*1.0)*((Math.sqrt(market.balance + budget)*a)*(Math.sqrt((market.market_employee + assigning)* b)))* c * rand(9000..11000)))
+    elsif market.market_employee < 50 
+      return Math.log(1 + (MARKETSIZE[market.market_master_id-2]*(MARKETSHARE[market.market_master_id-2]*1.5)*((Math.sqrt(market.balance + budget)*a)*(Math.sqrt((market.market_employee + assigning)* b)))* c * rand(9000..11000)))
+    elsif market.market_employee < 250
+      return Math.log(1 + (MARKETSIZE[market.market_master_id-2]*(MARKETSHARE[market.market_master_id-2]*2.0)*((Math.sqrt(market.balance + budget)*a)*(Math.sqrt((market.market_employee + assigning)* b)))* c * rand(9000..11000)))
+    elsif market.market_employee < 1250
+      return Math.log(1 + (MARKETSIZE[market.market_master_id-2]*(MARKETSHARE[market.market_master_id-2]*3.0)*((Math.sqrt(market.balance + budget)*a)*(Math.sqrt((market.market_employee + assigning)* b)))* c * rand(9000..11000)))
+    end
+  end
+  
+  def cal_market_employee
+    return market.market_employee + assigning
+  end
+  
+  def cal_balance(a)
+    return ( market.balance + budget ) * a
+  end
+end
   
   # def cal_params_market_earning
   #   if market.market_master_id == 1
@@ -304,91 +342,59 @@ class Investment < ApplicationRecord
   #   end
   # end
   
-  def cal_params_balance
-    # cal_value = [0.3,0.3,0.5,0.95,0.4,0.95,0.4,0.95,0.3,0.3,0.3,0.3,0.3,0.3,0.8,0.9,0.5,0.3,0.3]
-    # return cal_balance(cal_value[market.market_master_id])
-    if market.market_master_id == 1
-      return 0
-    elsif market.market_master_id == 2
-      ret_val = cal_balance(0.30)
-      return ret_val
-    elsif market.market_master_id == 3
-      ret_val = cal_balance(0.30)
-      return ret_val
-    elsif market.market_master_id == 4
-      ret_val = cal_balance(0.50)
-      return ret_val
-    elsif market.market_master_id == 5
-      ret_val = cal_balance(0.95)
-      return ret_val
-    elsif market.market_master_id == 6
-      ret_val = cal_balance(0.60)
-      return ret_val
-    elsif market.market_master_id == 7
-      ret_val = cal_balance(0.95)
-      return ret_val
-    elsif market.market_master_id == 8
-      ret_val = cal_balance(0.30)
-      return ret_val
-    elsif market.market_master_id == 9
-      ret_val = cal_balance(0.30)
-      return ret_val
-    elsif market.market_master_id == 10
-      ret_val = cal_balance(0.30)
-      return ret_val
-    elsif market.market_master_id == 11
-      ret_val = cal_balance(0.30)
-      return ret_val
-    elsif market.market_master_id == 12
-      ret_val = cal_balance(0.30)
-      return ret_val
-    elsif market.market_master_id == 13
-      ret_val = cal_balance(0.30)
-      return ret_val
-    elsif market.market_master_id == 14
-      ret_val = cal_balance(0.80)
-      return ret_val
-    elsif market.market_master_id == 15
-      ret_val = cal_balance(0.90)
-      return ret_val
-    elsif market.market_master_id == 16
-      ret_val = cal_balance(0.50)
-      return ret_val
-    elsif market.market_master_id == 17
-      ret_val = cal_balance(0.30)
-      return ret_val
-    elsif market.market_master_id == 18
-      ret_val = cal_balance(0.30)
-      return ret_val
-    end
-  end
-
-  def cal_market_recruiting(a,b) #a = 資本の効率性,b = 人的リソースの効率性
-    ret_val = Math.log(1 + ((budget*a)*((market.market_employee + assigning)*b*rand(5000..15000)) / 10000))
-    return ret_val
-  end
-
-  def cal_market_earning(a,b,c) #a = 資本の効率性,b = 人的リソースの効率性,c = 市場の成長性
-    if market.market_employee < 10
-      ret_val = Math.log(1 + (MARKETSIZE[market.market_master_id-2]*(MARKETSHARE[market.market_master_id-2]*1.0)*((Math.sqrt(market.balance + budget)*a)*(Math.sqrt((market.market_employee + assigning)* b)))* c * rand(9000..11000)))
-    elsif market.market_employee < 50 
-      ret_val = Math.log(1 + (MARKETSIZE[market.market_master_id-2]*(MARKETSHARE[market.market_master_id-2]*1.5)*((Math.sqrt(market.balance + budget)*a)*(Math.sqrt((market.market_employee + assigning)* b)))* c * rand(9000..11000)))
-    elsif market.market_employee < 250
-      ret_val = Math.log(1 + (MARKETSIZE[market.market_master_id-2]*(MARKETSHARE[market.market_master_id-2]*2.0)*((Math.sqrt(market.balance + budget)*a)*(Math.sqrt((market.market_employee + assigning)* b)))* c * rand(9000..11000)))
-    elsif market.market_employee < 1250
-      ret_val = Math.log(1 + (MARKETSIZE[market.market_master_id-2]*(MARKETSHARE[market.market_master_id-2]*3.0)*((Math.sqrt(market.balance + budget)*a)*(Math.sqrt((market.market_employee + assigning)* b)))* c * rand(9000..11000)))
-    end
-    return ret_val
-  end
-  
-  def cal_market_employee
-    ret_val = market.market_employee + assigning
-    return ret_val
-  end
-  
-  def cal_balance(a)
-    ret_val = ( market.balance + budget ) * a
-    return ret_val
-  end
-end
-
+  # def cal_params_balance
+  #   if market.market_master_id == 1
+  #     return 0
+  #   elsif market.market_master_id == 2
+  #     ret_val = cal_balance(0.30)
+  #     return ret_val
+  #   elsif market.market_master_id == 3
+  #     ret_val = cal_balance(0.30)
+  #     return ret_val
+  #   elsif market.market_master_id == 4
+  #     ret_val = cal_balance(0.50)
+  #     return ret_val
+  #   elsif market.market_master_id == 5
+  #     ret_val = cal_balance(0.95)
+  #     return ret_val
+  #   elsif market.market_master_id == 6
+  #     ret_val = cal_balance(0.60)
+  #     return ret_val
+  #   elsif market.market_master_id == 7
+  #     ret_val = cal_balance(0.95)
+  #     return ret_val
+  #   elsif market.market_master_id == 8
+  #     ret_val = cal_balance(0.30)
+  #     return ret_val
+  #   elsif market.market_master_id == 9
+  #     ret_val = cal_balance(0.30)
+  #     return ret_val
+  #   elsif market.market_master_id == 10
+  #     ret_val = cal_balance(0.30)
+  #     return ret_val
+  #   elsif market.market_master_id == 11
+  #     ret_val = cal_balance(0.30)
+  #     return ret_val
+  #   elsif market.market_master_id == 12
+  #     ret_val = cal_balance(0.30)
+  #     return ret_val
+  #   elsif market.market_master_id == 13
+  #     ret_val = cal_balance(0.30)
+  #     return ret_val
+  #   elsif market.market_master_id == 14
+  #     ret_val = cal_balance(0.80)
+  #     return ret_val
+  #   elsif market.market_master_id == 15
+  #     ret_val = cal_balance(0.90)
+  #     return ret_val
+  #   elsif market.market_master_id == 16
+  #     ret_val = cal_balance(0.50)
+  #     return ret_val
+  #   elsif market.market_master_id == 17
+  #     ret_val = cal_balance(0.30)
+  #     return ret_val
+  #   elsif market.market_master_id == 18
+  #     ret_val = cal_balance(0.30)
+  #     return ret_val
+  #   end
+  # end
