4c553d690e248ae7c9f84ae5dd21f1774d55b210
Taisei Uwami <taiseiuwami@TaiseinoMacBook-Pro.local>
2020-07-19 14:11:32 +0900

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@
 end
 #Banana
 (1..4).each do |num|
-  Team.create!(name: "Banana,Inc.", current_fund: 108, origin_fund: 108, investment_limit: 5*num)
+  Team.create!(name: "Banana,Inc.", current_fund: 100, origin_fund: 100, investment_limit: 5*num)
 end
 #Coconut
 (1..4).each do |num|
@@ -20,7 +20,7 @@ end
 end
 #Dragonfruit
 (1..4).each do |num|
-  Team.create!(name: "Dragonfruit,Inc.", current_fund: 100, origin_fund: 100, investment_limit: 5*num)
+  Team.create!(name: "Dragonfruit,Inc.", current_fund: 108, origin_fund: 108, investment_limit: 5*num)
 end
 
 Admin.create!(email: "admin@waqoo.jp", password: "adminadmin")
