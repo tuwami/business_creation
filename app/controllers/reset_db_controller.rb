afbf87fb1df09b040fed040e2efe657874450a42
Taisei Uwami <taiseiuwami@TaiseinoMacBook-Pro.local>
2021-02-28 11:51:04 +0900

    auto commit

--- app/controllers/reset_db_controller.rb
+++ app/controllers/reset_db_controller.rb
@@ -1,6 +1,16 @@
 class ResetDbController < ApplicationController
 
 	def reset
+		ApplicationRecord.delete_all
+		Investment.delete_all
+		Admin.delete_all
+		User.delete_all
+		UserTeam.delete_all
+		Team.delete_all
+		Market.delete_all
+		MarketMaster.delete_all
+		History.delete_all
+
 		`./reset_db_from_web.sh`
 		redirect_to teams_path
 	end
--- reset_db_from_web.sh
+++ reset_db_from_web.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./bin/rails db:migrate:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1 && ./bin/rails db:seed
+./bin/rails db:seed
