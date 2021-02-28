ff637b6ccc0521dc6c8cc9a74af36f919d43f764
Taisei Uwami <taiseiuwami@TaiseinoMacBook-Pro.local>
2021-02-28 11:59:04 +0900

    auto commit

--- app/controllers/reset_db_controller.rb
+++ app/controllers/reset_db_controller.rb
@@ -1,15 +1,14 @@
 class ResetDbController < ApplicationController
 
 	def reset
-		ApplicationRecord.delete_all
-		Investment.delete_all
-		Admin.delete_all
-		User.delete_all
-		UserTeam.delete_all
-		Team.delete_all
-		Market.delete_all
-		MarketMaster.delete_all
-		History.delete_all
+		Investment.destroy_all
+		Admin.destroy_all
+		User.destroy_all
+		UserTeam.destroy_all
+		Team.destroy_all
+		Market.destroy_all
+		MarketMaster.destroy_all
+		History.destroy_all
 
 		`./reset_db_from_web.sh`
 		redirect_to teams_path
