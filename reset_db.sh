9f0db36071cedd1191583c3caa351e077049712e
Taisei Uwami <taiseiuwami@TaiseinoMacBook-Pro.local>
2021-06-14 01:06:30 +0900

    auto commit

--- reset_db.sh
+++ reset_db.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-heroku pg:reset --confirm waqoo2020 -r heroku && heroku run rails db:migrate -r heroku && heroku run rails db:seed -r heroku
+heroku pg:reset --confirm waqoo2020 -r heroku-dev && heroku run rails db:migrate -r heroku-dev && heroku run rails db:seed -r heroku-dev
