5219c0b1a43d61fdcff64f774fb10782e27c5d37
Taisei Uwami <taiseiuwami@TaiseinoMacBook-Pro.local>
2021-08-06 17:30:35 +0900

    auto commit

--- db/schema.rb
+++ db/schema.rb
@@ -80,11 +80,15 @@ ActiveRecord::Schema.define(version: 2020_06_28_084406) do
   create_table "teams", force: :cascade do |t|
     t.string "name"
     t.float "current_fund", default: 100.0
-    t.integer "current_employee", default: 10
-    t.integer "current_novice", default: 9
+    t.integer "current_employee", default: 20
+    t.integer "current_novice", default: 19
     t.float "current_profit", default: 0.0
     t.float "origin_fund", default: 100.0
     t.integer "investment_limit", default: 20
+    t.float "current_fund_rollplay", default: 50.0
+    t.float "origin_fund_rollplay", default: 50.0
+    t.integer "current_employee_rollplay", default: 10
+    t.integer "cuurent_novice_rollplay", default: 9
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
